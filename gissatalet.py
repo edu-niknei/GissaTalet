4c70bd76cce8a77c66cecaca9f96cd6b97e62003
edu-niknei <nikolas.neimanis@skola.taby.se>
2025-11-06 12:21:00 +0100

    Update gissatalet.py
    Loop med flera försök

--- gissatalet.py
+++ gissatalet.py
@@ -3,8 +3,10 @@ print("Hej " + namn + " och välkommen till spelet!")
 
 import random
 hemligt_tal = random.randint(1, 10)
-gissning = int(input("Gissa ett tal mellan 1 och 10"))
-if gissning == hemligt_tal:
-    print("rätt gissat!")
-else:
-    print(f"tyvärr, det var {hemligt_tal}")
+gissning = None
+while gissning != hemligt_tal:
+    gissning = int(input("Gissa ett tal mellan 1 och 10"))
+    if gissning == hemligt_tal:
+        print("rätt gissat!")
+    else:
+        print("Fel gissat.")
